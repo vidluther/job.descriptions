84031525b10964a21634aebb0d41be3334960ab6
Vid Luther <vid@luther.io>
2023-02-16 21:47:29 -0600

    setup plausible analytics and tracking what jobs people are searching for.

--- src/pages/api/generate.js
+++ src/pages/api/generate.js
@@ -4,6 +4,7 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+import { usePlausible } from 'next-plausible'
 
 export default async function (req, res) {
   if (!configuration.apiKey) {
