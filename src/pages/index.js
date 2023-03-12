11490c1cc5acc7bd91438fc57732483850ea8f2e
Vid Luther <vid@luther.io>
2023-03-12 09:08:50 -0500

    * chore(package.json): remove @next/font dependency * fix(index.js): fix import statement for Inter font The @next/font dependency has been removed from package.json as it is no longer needed. The import statement for the Inter font has been fixed to use double quotes instead of single quotes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { useState,useEffect } from "react";
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { Inter } from "next/font/google"
 //import styles from '@/styles/Home.module.css'
 import Footer from 'components/Footer';
 
